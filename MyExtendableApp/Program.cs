c9f950f266e1d1d1c669a756f907c369d09644b5
Sergey Misko <spy_3462@mail.ru>
2022-07-21 16:57:13 +0400

    добавил метод LoadSnapin и набросал временный код

--- MyExtendableApp/Program.cs
+++ MyExtendableApp/Program.cs
@@ -9,7 +9,32 @@ namespace MyExtendableApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LoadSnapin();
+        }
+        static void LoadSnapin()
+        {
+            Console.WriteLine("Select assembly:\n");
+            string listOfAssembly = string.Format($"1 -> {Utilities.CommonSnappableTypesDLL}\n" +
+                $"2 -> {Utilities.CSharpSnapInDLL}\n" +
+                $"3 -> {Utilities.VbSnapInDLL}\n");
+            Console.WriteLine(listOfAssembly);
+            Console.Write("enter assembly number: ");
+            string asmChoice= Console.ReadLine();
+            switch(asmChoice)
+            {
+                case "1":
+                    ;
+                    break;
+                case "2":
+                    ;
+                    break;
+                case "3":
+                    ;
+                    break;
+                default:
+                    Console.WriteLine("Oops. Wrong choice");
+                    break;
+            }
         }
     }
 }
