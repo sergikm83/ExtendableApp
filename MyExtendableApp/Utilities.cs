76ed16c4f5c80069a3d358ec70ece3d46106f690
Sergey Misko <spy_3462@mail.ru>
2022-07-21 16:31:28 +0400

    добавил вспомогательный класс Utilities для удобства работы с путями и именами файлов проектов текущего решения

---
+++ MyExtendableApp/Utilities.cs
@@ -0,0 +1,23 @@
+﻿namespace MyExtendableApp
+{
+    // вспомогательный класс для упрощения работы
+    // с путями и именами файлов.
+    // Создан дополнительно и не входит в состав кода,
+    // описанного в книге, т.к. в качестве проекта MyExtedableApp
+    // была выбрана платформа NET.Core 5.0 вместо предлагаемой
+    // в книге .NET Framework версии 4.x
+    public static class Utilities
+    {
+        // Название папок с проектами решения ExtandableApp
+        public static readonly string MyExtendableAppDirectory = "MyExtendableApp";
+        public static readonly string CommonSnappableTypesDirectory = "CommonSnappableTypes";
+        public static readonly string CSharpSnapInDirectory = "CSharpSnapIn";
+        public static readonly string VbSnapInDirectory = "VbSnapIn";
+        // вспомогательный отрезок пути для всех проектов при компиляии
+        public static readonly string ProjectBinDebugNetDirectory = @"\bin\Debug\net5.0\";
+        // имена файлов библиотек классов решения ExtedableApp
+        public static readonly string CommonSnappableTypesDLL = "CommonSnappableTypes.dll";
+        public static readonly string CSharpSnapInDLL = "CSharpSnapIn.dll";
+        public static readonly string VbSnapInDLL = "VbSnapIn.dll";
+    }
+}
