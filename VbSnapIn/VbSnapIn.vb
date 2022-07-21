ec69338e8f2cfb17a10a75407c623f87b513df53
Sergey Misko <spy_3462@mail.ru>
2022-07-21 15:00:33 +0400

    описал класс VbSnapIn, реализующий интерфейс IAppFunctionality

--- VbSnapIn/VbSnapIn.vb
+++ VbSnapIn/VbSnapIn.vb
@@ -1,3 +1,10 @@
-﻿Public Class VbSnapIn
-    
+﻿Imports CommonSnappableTypes
+
+<CompanyInfo(CompanyName:="Chucky's Software", CompanyUrl:="www.ChuckySoft.com")>
+Public Class VbSnapIn
+    Implements IAppFunctionality
+
+    Public Sub DoIt() Implements IAppFunctionality.DoIt
+        Console.WriteLine("You have just used the VB snap in!")
+    End Sub
 End Class
